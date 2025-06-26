0d626fec0062bd11ba21ea958a0729f69286f2d6
Daniel Ugbomah <danielugbomaht@gmail.com>
2025-06-26 13:50:32 +0100

    `Refactor index.html, index.js, and style.css for Tic Tac Toe game`

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 var turn = 1;
 var boxes = document.querySelectorAll(".box");
-var board = ['', '', '', '', '', '', '', '', '',];
-var winnerDiv = document.querySelector(".winner")
+var board = ['', '', '', '', '', '', '', '', ''];
+var winnerDiv = document.querySelector(".winner");
 
-for( var i = 0; i < boxes.length; i++){
-  boxes[i].addEventListener('click', function(){
+for (var i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener('click', function () {
     var index = this.getAttribute('data-index');
 
-    if (boxes[index].innerHTML === '' && winnerDiv.innerHTML === ''){
-
-      if(turn === 1){
+    if (this.innerHTML === '' && winnerDiv.innerHTML === '') {
+      if (turn === 1) {
         this.innerHTML = "X";
-  
+        board[index] = "X";
         turn = 2;
-      }else{
+      } else {
         this.innerHTML = "O";
+        board[index] = "O";
         turn = 1;
-      }   
+      }
+
       checkWinner();
     }
-    
-  })
+  });
 }
 
-function checkWinner(){
+function checkWinner() {
   var winnerCheck = [
-    [0,1,2], [3,4,5], [6,7,8] //Rows
-    [0,3,6], [1,4,7], [2,5,8] //columns
-    [0,4,8], [2,4,6]          //Diagonals
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
   ];
-  //what if x is and o are on the same row  
-  for(var i = 0; i < winnerCheck[i].length; i++){
 
+  for (var i = 0; i < winnerCheck.length; i++) {
+    var [a, b, c] = winnerCheck[i];
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      winnerDiv.innerHTML = board[a] + " wins!";
+      return;
+    }
+  }
+
+  if (!board.includes('') && winnerDiv.innerHTML === '') {
+    winnerDiv.innerHTML = "It's a draw!";
   }
 }
